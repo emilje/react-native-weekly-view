af2978f67037ae9dadcf4dfad4ebac1c7d2535e7
Emil Jerkovic <emil.jerkovic@gmail.com>
2023-06-25 18:15:21 +0300

    Added images.

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView, View } from "react-native";
+import { SafeAreaView, StatusBar, View } from "react-native";
 import WeeklyView from "./src/WeeklyView";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { calendarEvent } from "./src/types";
@@ -7,42 +7,121 @@ import { calendarEvent } from "./src/types";
 const EVENTS: calendarEvent[] = [
   {
     id: 1,
-    isoStart: "2023-06-23T07:00:00.000+03:00",
-    isoEnd: "2023-06-23T08:00:00.000+03:00",
+    isoStart: "2023-06-19T07:00:00.000+03:00",
+    isoEnd: "2023-06-19T07:30:00.000+03:00",
     name: "Breakfast",
     icon: <MaterialCommunityIcons name="food" size={12} />,
-    disabled: true,
+    color:"lightsalmon",
+    disabled: false,
   },
   {
     id: 2,
-    isoStart: "2023-06-23T08:30:00.000+03:00",
-    isoEnd: "2023-06-23T09:00:00.000+03:00",
-    name: "Lunch",
-    icon: <MaterialCommunityIcons name="food" size={12} />,
-    color: "pink",
+    isoStart: "2023-06-19T08:00:00.000+03:00",
+    isoEnd: "2023-06-19T08:45:00.000+03:00",
+    name: "Commute",
+    icon: <MaterialCommunityIcons name="bus" size={12} />,
     disabled: false,
   },
   {
     id: 3,
-    isoStart: "2023-06-23T12:00:00.000+03:00",
-    isoEnd: "2023-06-23T13:30:00.000+03:00",
-    name: "Coding",
+    isoStart: "2023-06-19T09:00:00.000+03:00",
+    isoEnd: "2023-06-19T12:00:00.000+03:00",
+    name: "Work",
     icon: <MaterialCommunityIcons name="laptop" size={12} />,
+    color:"crimson",
     disabled: false,
   },
   {
     id: 4,
-    isoStart: "2023-06-19T07:15:00.000+03:00",
-    isoEnd: "2023-06-19T08:00:00.000+03:00",
-    name: "Monday stuff",
-    icon: <MaterialCommunityIcons name="car" size={12} />,
+    isoStart: "2023-06-19T10:00:00.000+03:00",
+    isoEnd: "2023-06-19T10:30:00.000+03:00",
+    name: "Meeting",
+    color:"crimson",
+    icon: <MaterialCommunityIcons name="laptop" size={12} />,
     disabled: false,
   },
   {
     id: 5,
-    isoStart: "2023-06-19T07:20:00.000+03:00",
-    isoEnd: "2023-06-19T08:45:00.000+03:00",
-    name: "Monday stuff 2",
+    isoStart: "2023-06-19T12:30:00.000+03:00",
+    isoEnd: "2023-06-19T13:20:00.000+03:00",
+    name: "Commute",
+    icon: <MaterialCommunityIcons name="bus" size={12} />,
+    disabled: false,
+  },
+  {
+    id: 6,
+    isoStart: "2023-06-19T14:00:00.000+03:00",
+    isoEnd: "2023-06-19T15:30:00.000+03:00",
+    name: "Movies",
+    icon: <MaterialCommunityIcons name="movie" size={12} />,
+    disabled: false,
+  },
+  {
+    id: 7,
+    isoStart: "2023-06-21T07:00:00.000+03:00",
+    isoEnd: "2023-06-21T07:30:00.000+03:00",
+    name: "Breakfast",
+    icon: <MaterialCommunityIcons name="food" size={12} />,
+    color:"lightsalmon",
+    disabled: false,
+  },
+  {
+    id: 8,
+    isoStart: "2023-06-21T08:30:00.000+03:00",
+    isoEnd: "2023-06-21T10:30:00.000+03:00",
+    name: "Gym",
+    icon: <MaterialCommunityIcons name="weight" size={12} />,
+    disabled: false,
+  },
+  {
+    id: 9,
+    isoStart: "2023-06-21T11:15:00.000+03:00",
+    isoEnd: "2023-06-21T12:00:00.000+03:00",
+    name: "Sauna",
+    icon: <MaterialCommunityIcons name="cloud" size={12} />,
+    disabled: false,
+  },
+  {
+    id: 10,
+    isoStart: "2023-06-21T13:00:00.000+03:00",
+    isoEnd: "2023-06-21T14:00:00.000+03:00",
+    name: "Gaming",
+    icon: <MaterialCommunityIcons name="laptop" size={12} />,
+    disabled: false,
+  },
+  {
+    id: 11,
+    isoStart: "2023-06-24T09:00:00.000+03:00",
+    isoEnd: "2023-06-24T09:30:00.000+03:00",
+    name: "Breakfast",
+    icon: <MaterialCommunityIcons name="food" size={12} />,
+    color:"lightsalmon",
+    disabled: false,
+  },
+  {
+    id: 12,
+    isoStart: "2023-06-24T10:00:00.000+03:00",
+    isoEnd: "2023-06-24T12:00:00.000+03:00",
+    name: "Beach time",
+    icon: <MaterialCommunityIcons name="beach" size={12} />,
+    disabled: false,
+  },
+  {
+    id: 13,
+    isoStart: "2023-06-24T12:30:00.000+03:00",
+    isoEnd: "2023-06-24T15:00:00.000+03:00",
+    name: "Family dinner",
+    icon: <MaterialCommunityIcons name="food-fork-drink" size={12} />,
+    color:"lightsalmon",
+    disabled: false,
+  },
+  {
+    id: 14,
+    isoStart: "2023-06-24T07:00:00.000+03:00",
+    isoEnd: "2023-06-24T08:55:00.000+03:00",
+    name: "Extra sleep",
+    icon: <MaterialCommunityIcons name="bed" size={12} />,
+    color:"cornflowerblue",
     disabled: false,
   },
 ];
@@ -50,27 +129,26 @@ const EVENTS: calendarEvent[] = [
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+      <StatusBar barStyle={"light-content"} />
       <View style={{ flex: 1, flexDirection: "row" }}>
         <View style={{ flex: 1 }}>
           <WeeklyView
-            timetableStartHour={5}
-            intervalHeight={15}
-            timezone="Europe/Zagreb"
+            timezone="Europe/Helsinki"
             events={EVENTS}
             onEventPress={(event: calendarEvent) => {
               console.log("Pressed", event);
             }}
           />
-          <WeeklyView
+          {/* <WeeklyView
             intervalLengthMinutes={60}
             theme="light"
             events={EVENTS}
             onEventPress={(event: calendarEvent) => {
               console.log("Pressed", event);
             }}
-          />
+          /> */}
         </View>
-        <View style={{ flex: 1 }}>
+        {/* <View style={{ flex: 1 }}>
           <WeeklyView
             theme="dark"
             style={{ headerTextColor: "pink", timetableTextColor: "lightblue" }}
@@ -86,7 +164,7 @@ export default function App() {
               console.log("Pressed", event);
             }}
           />
-        </View>
+        </View> */}
       </View>
     </SafeAreaView>
   );
